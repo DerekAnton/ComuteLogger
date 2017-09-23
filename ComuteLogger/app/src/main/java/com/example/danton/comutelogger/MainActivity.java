953ba252308f98d97a8ecf01ba1bf7c591b24453
DerekAnton <danton5276@gmail.com>
2017-09-23 18:26:54 -0400

    Added functionality of basic stopwatch, start, pause, stop
      - Start and pause fully functional, stop is a placeholder.
      - Plans to make stop log to value, and reset the counter to zero,
        assuming the commute has ended.
      - Removed useless swap files
      - Plans to add database interaction, or at the very least a flat file
        where the program can dump the data too with information about the
        ride. i.e. time of day, day of the week, etc.

--- ComuteLogger/app/src/main/java/com/example/danton/comutelogger/MainActivity.java
+++ ComuteLogger/app/src/main/java/com/example/danton/comutelogger/MainActivity.java
@@ -1,15 +1,92 @@
 package com.example.danton.comutelogger;
 
+import android.content.Context;
+import android.os.Handler;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity
 {
+
+
+    Button btnStart, btnPause, btnStop;
+    TextView txtTimer;
+    Handler customHandler = new Handler();
+    LinearLayout container;
+
+    long startTime = 0L, timeMs = 0L, timeSwapBuff = 0L, updateTime= 0L;
+
+    Runnable updateTimerThread = new Runnable()
+    {
+        @Override
+        public void run()
+        {
+            timeMs      = SystemClock.uptimeMillis() - startTime;
+            updateTime  = timeSwapBuff + timeMs;
+            int seconds = (int)(updateTime / 1000);
+            int minutes = seconds/60;
+            int hours   = minutes/60;
+            int milliseconds = (int)(updateTime % 1000);
+            txtTimer.setText("" + minutes + ":" +
+                             "" + String.format("%02d", seconds) + ":" +
+                             "" + String.format("%03d", milliseconds)
+            );
+
+            customHandler.postDelayed(this, 0);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnStart  = (Button)findViewById(R.id.startButton);
+        btnPause  = (Button)findViewById(R.id.pauseButton);
+        btnStop   = (Button)findViewById(R.id.stopButton);
+        txtTimer  = (TextView)findViewById(R.id.timerValue);
+        container = (LinearLayout)findViewById(R.id.container);
+
+        btnStart.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                startTime = SystemClock.uptimeMillis();
+                customHandler.postDelayed(updateTimerThread, 0);
+            }
+        });
+
+        btnPause.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                timeSwapBuff += timeMs;
+                customHandler.removeCallbacks(updateTimerThread);
+            }
+        });
+
+        btnStop.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                LayoutInflater inflater = (LayoutInflater)getBaseContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                View addView = inflater.inflate(R.layout.raw, null);
+                TextView txtValue = (TextView)addView.findViewById(R.id.txtContent);
+                txtValue.setText(txtTimer.getText());
+                container.addView(addView);
+            }
+        });
+
     }
 }
